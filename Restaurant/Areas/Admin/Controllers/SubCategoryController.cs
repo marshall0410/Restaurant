ec456711693e3cd6fc5ee76809baa8054fde1d59
Marshall Jones <jones.marshall0410@gmail.com>
2019-03-01 11:04:10 -0500

    added SubCategory viewmodel

--- Restaurant/Areas/Admin/Controllers/SubCategoryController.cs
+++ Restaurant/Areas/Admin/Controllers/SubCategoryController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Restaurant.Data;
 using Restaurant.Models;
+using Restaurant.Models.ViewModels;
 
 namespace Restaurant.Areas.Admin.Controllers
 {
@@ -24,5 +25,15 @@ namespace Restaurant.Areas.Admin.Controllers
             return View(subCategory);
         }
 
+        public async Task<IActionResult> Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = new SubCategory(),
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
+            };
+            return View(model);
+        }
     }
 }
